120ecbb372db41d48ebc52395d28d4dfe47661db
dewansh59 <dewansh59@pop-os.localdomain>
2021-01-18 23:36:22 +0530

    added video preview

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import {createFFmpeg,fetchFile} from '@ffmpeg/ffmpeg';
 import { useEffect,useState } from 'react';
 const ffmpeg = createFFmpeg({log:true});
 function App() {
-  const [ready, setready] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
   const load = async ()=> {
     await ffmpeg.load();
-    setready(true); 
+    setReady(true); 
   }
   useEffect(()=>{
     load();
   },[])
-  return (
+  const convertToGif = async()=>{
+    ffmpeg.FS('writeFile','test.mp4',await fetchFile(video));
+  }
+  return ready ? (
     <div className="App">
-      <h1>hi</h1>
+      {video && <video
+                controls
+                width="250"
+                src={URL.createObjectURL(video)}   
+                ></video>}
 
+    <input type="file" onChange={(e)=>setVideo(e.target.files?.item(0))}/>
     </div>
-  );
+  ):
+  (<p>Loading...</p>)
 }
 
 export default App;
