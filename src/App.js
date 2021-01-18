40d21d630566713f6a5bd3fd0e94e7690accd83b
dewansh59 <dewansh59@pop-os.localdomain>
2021-01-18 23:50:36 +0530

    convert to gif

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ function App() {
   },[])
   const convertToGif = async()=>{
     ffmpeg.FS('writeFile','test.mp4',await fetchFile(video));
+    await ffmpeg.run('-i','test.mp4','-t','10','-ss','2.0','-f','gif','out.gif');
+    const data = ffmpeg.FS('readFile','out.gif');
+    const url = URL.createObjectURL(new Blob([data.buffer],{type:'image/gif'}));
+    setGif(url);
   }
   return ready ? (
     <div className="App">
@@ -25,6 +29,8 @@ function App() {
                 ></video>}
 
     <input type="file" onChange={(e)=>setVideo(e.target.files?.item(0))}/>
+    <button onClick={convertToGif}>Convert</button>
+    { gif && <img src={gif}/> }
     </div>
   ):
   (<p>Loading...</p>)
